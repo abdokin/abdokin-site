9e2006123a458b608cf39c5f336565c5dcfb1dff
abdokin <bashbin.sh@gmail.com>
2023-11-19 00:46:55 +0100

    fix meta tag in index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,8 +4,8 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Abderrahman Laraich Home" },
+    { name: "description", content: "Welcome to my website!" },
   ];
 };
 
