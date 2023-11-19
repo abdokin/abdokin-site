5bae90daaa441df6ad741220151c16517860141b
abdokin <bashbin.sh@gmail.com>
2023-11-19 18:02:02 +0100

    add site config

---
+++ app/config/site.ts
@@ -0,0 +1,18 @@
+const siteConfig = {
+    name: 'Abderrahman Laraich',
+    logo: 'https://example.com/logo.png',
+    description: 'This is a fantastic website!',
+    contactEmail: 'laarichabdo@gmail.com.com',
+    address:"QUARTIER ESSAFAE 1 IMM L4 RDC NR 2 BOUSKOURA CASA",
+    phone:"+212614292371",
+    country: "Moroco",
+    city: "Casablanca",
+    socialMedia: {
+      twitter: 'https://twitter.com/example',
+      facebook: 'https://facebook.com/example',
+      instagram: 'https://instagram.com/example',
+    },
+  };
+  
+  export default siteConfig;
+  
