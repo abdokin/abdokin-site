7d7238bce7012b7468eb1f379e904947822afd59
abdokin <bashbin.sh@gmail.com>
2023-11-19 18:01:26 +0100

    create main footer

---
+++ app/components/main-footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className=" p-4 text-center container-lg">
+      <p className="text-sm ">
+        © {new Date().getFullYear()} abdokin. All Rights Reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
