c0e36b7a6fcba6ad9d08d43f4df85b84cfc790fd
abdokin <bashbin.sh@gmail.com>
2023-11-21 18:03:34 +0100

    animate the index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,10 @@
+import { motion } from 'framer-motion';
 import { MainNav } from "~/components/main-nav";
 import { Button } from "~/components/ui/button";
 import type { MetaFunction } from "@remix-run/node";
 import HireMe from "~/components/hire-me-card";
+import config from '../config/site';
+import { AnimatedText } from "~/components/animated-text";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,37 +12,99 @@ export const meta: MetaFunction = () => {
     { name: "description", content: "Welcome to my website!" },
   ];
 };
-import config from '../config/site'
+
 export default function Index() {
   return (
-    <div className="container-lg w-full ">
+    <motion.div
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -50 }}
+      transition={{ duration: 0.5 }}
+      className="container-lg w-full"
+    >
       <div className="py-4">
-        <h1 className="text-2xl md:text-4xl font-bold">Hi, I’m {config.name}</h1>
-        <p className="pt-4 text-md text-muted-foreground">I am a developer, an <u className="text-black dark:text-white">open source </u>lover and a hand-tool woodworker from {config.country}.</p>
+        <motion.h1
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          className="text-2xl md:text-4xl font-bold"
+        >
+          Hi, I’m{' '}
+          <AnimatedText text={config.name} once={true} el="span" />
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          className="pt-4 text-md text-muted-foreground"
+        >
+          I am a developer, an{' '}
+          <u className="text-black dark:text-white">open source </u>
+          lover and a hand-tool woodworker from {config.country}.
+        </motion.p>
 
-        <p className="pt-4 text-md text-muted-foreground">
+        <motion.p
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.6, duration: 0.5 }}
+          className="pt-4 text-md text-muted-foreground"
+        >
           This is my digital garden, where I write about the things I’m working on and share what I’ve learned.
-        </p>
+        </motion.p>
       </div>
       <HireMe />
 
-      <div>
-        <h1 className="text-2xl font-bold py-4">About Me</h1>
-        <img src="/casablanca-2.jpeg" />
-        <p className="pt-4 text-md text-muted-foreground">
-          I live in {config.city}, in the {config.country}, with my mother and my sister.
-        </p>
-        <p className="pt-4 text-md text-muted-foreground">
-          My hobbies vary a lot based on my current interests, but playing music (piano/synths & guitar) is always a constant.
-        </p>
-        <p className="pt-4 text-md text-muted-foreground">
-          I’m currently learning woodworking to build toys for my kids, and take sunrise hikes to enjoy the surrounding mountains.
-        </p>
-        <p className="pt-4 text-md text-muted-foreground">
-          I also love cycling, both for transportation and tourism. With my friends, 
-          I’ve toured across 7 countries on my bike, along the French Atlantic coast, in the Alps and in the Balkans:
-        </p>
-      </div>
-    </div>
+
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 1, duration: 0.5 }}
+          className="py-4"
+        >
+          <h1 className="text-2xl font-bold py-4">About Me</h1>
+          <motion.img
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            src="/casablanca-2.jpeg"
+            alt="Casablanca"
+            className="w-full"
+          />
+          <motion.p
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.5, duration: 0.5 }}
+            className="pt-4 text-md text-muted-foreground"
+          >
+            I live in {config.city}, in the {config.country}, with my mother and my sister.
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.7, duration: 0.5 }}
+            className="pt-4 text-md text-muted-foreground"
+          >
+            My hobbies vary a lot based on my current interests, but playing music (piano/synths & guitar) is always a constant.
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.9, duration: 0.5 }}
+            className="pt-4 text-md text-muted-foreground"
+          >
+            I’m currently learning woodworking to build toys for my kids, and take sunrise hikes to enjoy the surrounding mountains.
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 1.1, duration: 0.5 }}
+            className="pt-4 text-md text-muted-foreground"
+          >
+            I also love cycling, both for transportation and tourism. With my friends,
+            I’ve toured across 7 countries on my bike, along the French Atlantic coast, in the Alps and in the Balkans.
+          </motion.p>
+      </motion.div>
+    </motion.div>
+
   );
 }
